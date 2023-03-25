b1c49aa4f6720a67ef21e9b33ea37cf23aee6469
Alex Blizzard <alexblizzard2@gmail.com>
2023-03-24 21:26:03 -0400

    Render deployment

---
+++ server.ts
@@ -0,0 +1,44 @@
+"use strict";
+
+//IMPORTS
+import http from 'http';
+import fs from 'fs';
+import mime from 'mime-types';
+
+//VARIABLES
+const port = process.env.PORT || 3000;
+let lookup = mime.lookup;
+
+//CONFIGURATIONS
+
+//STATIC CONFIGURATIONS
+
+const server = http.createServer((req, res) => {
+
+    let path : string = req.url as string;
+    if(path === "/" || path === "/home"){
+        path = "/index.html"
+    }
+
+    console.log(__dirname);
+
+    let mime_type : string = lookup(path.substring(1)) as string
+
+    fs.readFile(__dirname + path, function (err, data){
+
+        if (err){
+            res.writeHead(404);
+            res.end("Error 404, file not found" + err.message);
+            return;
+        }
+        //used as a means to prevent users from changing the content-type
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        res.writeHead(200, {'Content-Type' : mime_type})
+        res.end(data);
+    });
+
+});
+
+server.listen(port, () => {
+    console.log(`Server running at: ${port}/`);
+});
